e0c2d142aa2500e524a06bc942761770f2e9bfb7
Etienne <etienne.moureton@unflux.fr>
2022-08-10 11:40:17 +0200

    add favicon

---
+++ components/partials/DashedLines.jsx
@@ -0,0 +1,7 @@
+export default function DashedLines() {
+  return (
+    <div className='absolute h-full w-full max-w-[128rem] mx-auto top-0 left-0 right-0 grid grid-cols-4 justify-center border-r-1 border-dashed border-[#404040]'>
+      {Array.from({ length: 4 }, (_, i) => <div className='h-full border-l-1 border-dashed border-[#404040]' key={i}></div>)}
+    </div>
+  )
+}
--- components/sections/Technos.jsx
+++ components/sections/Technos.jsx
@@ -2,14 +2,13 @@ import ReactIcon from '../../assets/svg/technos/react.svg'
 import VueIcon from '../../assets/svg/technos/vue.svg'
 import NextIcon from '../../assets/svg/technos/next.svg'
 import SvelteIcon from '../../assets/svg/technos/svelte.svg'
+import DashedLines from '../partials/DashedLines'
 
 
 export default function Technos() {
   return (
     <section className="h-[230rem] bg-greyDark mt-[-55rem] relative">
-      <div className='absolute h-full w-full max-w-[128rem] mx-auto top-0 left-0 right-0 grid grid-cols-4 justify-center border-r-1 border-dashed border-[#404040]'>
-        {Array.from({ length: 4 }, (_, i) => <div className='h-full border-l-1 border-dashed border-[#404040]' key={i}></div>)}
-      </div>
+      <DashedLines />
       <div className="grid grid-cols-4 justify-center h-full absolute top-0 left-0 right-0 max-w-[128rem] mx-auto pt-[36rem]">
         <div className="flex flex-col items-center pt-[25rem]">
           <ReactIcon className="h-[5.5rem] mb-[35.7rem] drop-shadow-white" />
