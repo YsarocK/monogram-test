e895a84d6da3b7ef67ad73b37d8b2333894da373
Etienne <etienne.moureton@unflux.fr>
2022-08-10 11:35:41 +0200

    add technos logo, change lines & add logos shadows

--- components/sections/Technos.jsx
+++ components/sections/Technos.jsx
@@ -6,22 +6,26 @@ import SvelteIcon from '../../assets/svg/technos/svelte.svg'
 
 export default function Technos() {
   return (
-    <section className="h-[230rem] bg-greyDark mt-[-50rem] relative">
-      <div className="grid justify-center h-full absolute top-0 left-0 right-0 grid-cols-4 max-w-[128rem] mx-auto pt-[36rem]">
-        <div className="flex flex-col items-center gap-[35.7rem] border-l-1 border-dashed border-[#404040]">
-          <ReactIcon />
-          <SvelteIcon />
+    <section className="h-[230rem] bg-greyDark mt-[-55rem] relative">
+      <div className='absolute h-full w-full max-w-[128rem] mx-auto top-0 left-0 right-0 grid grid-cols-4 justify-center border-r-1 border-dashed border-[#404040]'>
+        {Array.from({ length: 4 }, (_, i) => <div className='h-full border-l-1 border-dashed border-[#404040]' key={i}></div>)}
+      </div>
+      <div className="grid grid-cols-4 justify-center h-full absolute top-0 left-0 right-0 max-w-[128rem] mx-auto pt-[36rem]">
+        <div className="flex flex-col items-center pt-[25rem]">
+          <ReactIcon className="h-[5.5rem] mb-[35.7rem] drop-shadow-white" />
+          <SvelteIcon className="h-[5.5rem] opacity-40 drop-shadow-white" />
         </div>
-        <div className="flex flex-col items-center gap-[50rem] border-l-1 border-dashed border-[#404040]">
-          <NextIcon />
-          <ReactIcon />
+        <div className="flex flex-col items-center pt-[38rem]">
+          <NextIcon className="h-[7.5rem] mb-[50rem] drop-shadow-white" />
+          <ReactIcon className="h-[5.5rem] opacity-10 drop-shadow-white" />
         </div>
-        <div className="flex flex-col items-center border-l-1 border-dashed border-[#404040]">
-          <VueIcon />
+        <div className="flex flex-col items-center pt-[72rem]">
+          <VueIcon className="h-[6.5rem] opacity-40 drop-shadow-white" />
         </div>
-        <div className="flex flex-col items-centerborder-x-1 border-dashed border-[#404040]">
-          <NextIcon />
-          <ReactIcon />
+        <div className="flex flex-col items-center">
+          <NextIcon className="h-[7.5rem] mb-[40rem] drop-shadow-white" />
+          <ReactIcon className="h-[5.5rem] mb-[58rem] drop-shadow-white" />
+          <SvelteIcon className="h-[5.5rem] opacity-10 drop-shadow-white" />
         </div>
       </div>
     </section>
