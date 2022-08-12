adbd787259b325c2a6c7faa4516172f354cabd71
Etienne <etienne.moureton@unflux.fr>
2022-08-12 09:06:54 +0200

    change technos/javascript section organisation

--- components/sections/Computer.js
+++ components/sections/Computer.js
@@ -7,13 +7,15 @@ import DashedLines from "../partials/DashedLines"
 export default function Computer() {
   return (
     <section className="mt-[4.2rem] text-center relative 2xl:container px-24 mx-auto z-10 bg-greyDark md:bg-[transparent] py-20 md:py-0">
-      <DashedLines className="md:hidden" />
+      <div className="bg-greyDark absolute w-[100vw] h-[75%] xl:h-[55%] lg:max-h-[55rem] bottom-0 left-2/4 -translate-x-2/4">
+        <DashedLines />
+      </div>
       <CardBig className="absolute z-[0] lg:z-[0] left-0 right-0 w-full px-24" />
       <Line className="absolute top-[22rem] lg:top-[26rem] right-[10rem] w-[15rem] lg:w-[24.8rem]" />
       <div className="pt-[9rem] flex flex-col items-center relative">
         <Title Tag="h2" Text="javascript" className="text-white tracking-[-0.039rem]" />
         <p className="text-base font-light text-white leading-base max-w-[57.8rem] mt-[4.4rem] mb-[9rem]">Statically generated pages are brought to life with client-side JavaScript libraries and frameworks, such as Algolia and Next.js.</p>
-        <RetroComputer className="mx-24 w-full lg:w-[80rem]" />
+        <RetroComputer className="mx-24 w-[65vw] lg:w-[80rem]" />
       </div>
     </section>
   )
--- components/sections/Technos.js
+++ components/sections/Technos.js
@@ -7,9 +7,9 @@ import DashedLines from '../partials/DashedLines'
 
 export default function Technos() {
   return (
-    <section className="bg-greyDark my-[-1rem] pb-[1rem] md:mt-[-55rem] relative">
+    <section className="bg-greyDark mb-[-1rem] pb-[1rem] relative z-10">
       <DashedLines />
-      <div className="grid grid-cols-4 justify-center h-full max-w-[128rem] mx-auto md:pt-[36rem]">
+      <div className="grid grid-cols-4 justify-center h-full max-w-[128rem] mx-auto">
         <div className="flex flex-col items-center pt-[25rem]">
           <ReactIcon className="w-[70%] md:w-[auto] md:h-[5.5rem] mb-[35.7rem] drop-shadow-white" />
           <SvelteIcon className="w-[70%] md:w-[auto] md:h-[5.5rem] opacity-40 drop-shadow-white" />
