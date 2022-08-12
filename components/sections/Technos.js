c7fa7133438d66ae794daeb28ce953b6bb68a3c5
Etienne <etienne.moureton@unflux.fr>
2022-08-12 10:24:36 +0200

    responsive technos section

--- components/sections/Technos.js
+++ components/sections/Technos.js
@@ -10,20 +10,20 @@ export default function Technos() {
     <section className="bg-greyDark mb-[-1rem] pb-[1rem] relative z-10">
       <DashedLines />
       <div className="grid grid-cols-4 justify-center h-full max-w-[128rem] mx-auto">
-        <div className="flex flex-col items-center pt-[25rem]">
-          <ReactIcon className="w-[70%] md:w-[auto] md:h-[5.5rem] mb-[35.7rem] drop-shadow-white" />
+        <div className="flex flex-col items-center pt-[7.5rem] md:pt-[25rem]">
+          <ReactIcon className="w-[70%] md:w-[auto] md:h-[5.5rem] mb-[12rem] md:mb-[35.7rem] drop-shadow-white" />
           <SvelteIcon className="w-[70%] md:w-[auto] md:h-[5.5rem] opacity-40 drop-shadow-white" />
         </div>
-        <div className="flex flex-col items-center pt-[38rem]">
-          <NextIcon className="w-[70%] md:w-[auto] md:h-[7.5rem] mb-[50rem] drop-shadow-white" />
+        <div className="flex flex-col items-center pt-[10rem] md:pt-[38rem]">
+          <NextIcon className="w-[70%] md:w-[auto] md:h-[7.5rem] mb-[15rem] md:mb-[50rem] drop-shadow-white" />
           <ReactIcon className="w-[70%] md:w-[auto] md:h-[5.5rem] opacity-10 drop-shadow-white" />
         </div>
-        <div className="flex flex-col items-center pt-[72rem]">
+        <div className="flex flex-col items-center pt-[18rem] md:pt-[72rem]">
           <VueIcon className="w-[70%] md:w-[auto] md:h-[6.5rem] opacity-40 drop-shadow-white" />
         </div>
         <div className="flex flex-col items-center">
-          <NextIcon className="w-[70%] md:w-[auto] md:h-[7.5rem] mb-[40rem] drop-shadow-white" />
-          <ReactIcon className="w-[70%] md:w-[auto] md:h-[5.5rem] mb-[58rem] drop-shadow-white" />
+          <NextIcon className="w-[70%] md:w-[auto] md:h-[7.5rem] mb-[13rem] md:mb-[40rem] drop-shadow-white" />
+          <ReactIcon className="w-[70%] md:w-[auto] md:h-[5.5rem] mb-[18rem] md:mb-[58rem] drop-shadow-white" />
           <SvelteIcon className="w-[70%] md:w-[auto] md:h-[5.5rem] opacity-10 drop-shadow-white" />
         </div>
       </div>
