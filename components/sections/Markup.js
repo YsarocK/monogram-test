a8f84d006fed211aae64ffc29aba50a4e4ba10e8
Etienne <etienne.moureton@unflux.fr>
2022-08-12 09:57:16 +0200

    fix some backdrop filter behavior

--- components/sections/Markup.js
+++ components/sections/Markup.js
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import Title from "../partials/Title";
-import PrimaryRectangle from '../../assets/img/markup__rectangle__primary.png'
-import SecondaryRectangle from '../../assets/img/markup__rectangle__secondary.png'
+import PrimaryRectangle from '../../assets/svg/markup__rectangle__primary.svg'
+import SecondaryRectangle from '../../assets/svg/markup__rectangle__secondary.svg'
 
 export default function Markup() {
   return (
     <section className="flex flex-col items-center relative">
-      <div className="flex justify-center lg:justify-start 2xl:container w-full px-24 mx-auto relative">
+      <div className="flex justify-center xl:justify-start 2xl:container w-full px-24 mx-auto relative">
         <div className="flex flex-col items-center lg:items-start gap-16">
           <div className="flex gap-11">
             <Title Tag="h1" Text="markup" />
           </div>
-          <p className="text-[2rem] sm:text-[2.4rem] max-w-[48rem] font-normal text-[#414141] leading-base text-center lg:text-left">
+          <p className="text-[2rem] sm:text-[2.4rem] max-w-[48rem] font-normal text-[#414141] leading-base text-center xl:text-left">
             When ready for deployment, a static-site generator such as Astro or Next.js is used to compile the website. The end result is a collection of pre-rendered HTML pages that can be delivered lightning-fast over a CDN like Vercel’s Edge Network.
           </p>
           <div className="relative w-full mt-[8rem] h-[50rem]">
@@ -21,26 +21,14 @@ export default function Markup() {
           </div>
         </div>
         {/* Decided to hide the cards on mobile. Too color for a small devices (personal point of vue, I agree). */}
-        <div className="hidden z-[-1] lg:flex gap-x-[4.6rem] absolute right-0">
-          <div className="pt-[17rem] relative z-10">
-            <Image
-              src={PrimaryRectangle}
-              alt="Hero Abstract Rectangle"
-              width={280}
-              height={670}
-              layout="fixed"
-            />
+        <div className="hidden z-[-1] xl:flex gap-x-[4.6rem] absolute right-0">
+          <div className="pt-[17rem] relative z-10 w-[28rem] h-[63rem]">
+            <PrimaryRectangle />
             <div className="w-[28rem] h-[47rem] absolute top-[48rem] left-[16.4rem] [backdrop-filter:blur(120px)] rounded-[6.8rem] z-1"></div>
           </div>
-          <div className="relative">
-            <Image
-              src={SecondaryRectangle}
-              alt="Hero Abstract Rectangle"
-              width={278}
-              height={670}
-              layout="fixed"
-            />
-            <div className="w-[28rem] h-[66rem] absolute top-[30rem] left-[15.8rem] [backdrop-filter:blur(120px)] rounded-[6.8rem]"></div>
+          <div className="relative w-[28rem] h-[66rem]">
+            <SecondaryRectangle />
+            <div className="w-full h-full absolute top-[30rem] left-[15.8rem] [backdrop-filter:blur(120px)] rounded-[6.8rem]"></div>
           </div>
         </div>
       </div>
